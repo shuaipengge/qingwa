5258bb0fe1eed8c3820726977ea3d6728556d7df
shuaipengge <shuaipengge@gmail.com>
2020-02-02 15:22:52 +0800

    使用 avue 实现 crud

--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
-import CourseList from '../views/courses/CourseList.vue'
-import CourseEdit from '../views/courses/CourseEdit.vue'
+import CourseCrud from '../views/courses/CourseCrud.vue'
+// import CourseList from '../views/courses/CourseList.vue'
+// import CourseEdit from '../views/courses/CourseEdit.vue'
 
 Vue.use(VueRouter)
 
@@ -13,9 +14,11 @@ const routes: RouteConfig[] = [
     component: Main,
     children: [
       { name: 'home', path: '/', component: Home },
-      { name: 'courses-list', path: '/courses/list', component: CourseList },
-      { name: 'courses-edit', path: '/courses/edit/:id', component: CourseEdit, props: true },
-      { name: 'courses-create', path: '/courses/create', component: CourseEdit }
+      { name: 'courese-crud', path: '/courses/list', component: CourseCrud }
+
+      // { name: 'courses-list', path: '/courses/list', component: CourseList }
+      // { name: 'courses-edit', path: '/courses/edit/:id', component: CourseEdit, props: true },
+      // { name: 'courses-create', path: '/courses/create', component: CourseEdit }
     ]
   }
 ]
