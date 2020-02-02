cc7314c769c71788e1629246c79f039fa7c1a077
shuaipengge <shuaipengge@gmail.com>
2020-02-02 21:58:13 +0800

    使用 阿里云OSS 云存储

--- server/apps/admin/src/app.module.ts
+++ server/apps/admin/src/app.module.ts
@@ -6,10 +6,18 @@ import { UsersModule } from './users/users.module';
 import { CoursesModule } from './courses/courses.module';
 import { EpisodesModule } from './episodes/episodes.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { ossconfig } from './app.keys'
+
+const MAO = require('multer-aliyun-oss');
 
 @Module({
   imports: [
-    MulterModule.register({ dest: 'uploads' }), // 文件上传
+    MulterModule.register({
+      storage: MAO({
+        config: ossconfig
+      })
+      // dest: 'uploads' 
+    }), // 文件上传
     DbModule,
     UsersModule,
     CoursesModule,
